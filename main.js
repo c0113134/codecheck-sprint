533752d7f24f9558f5b0235df761ef65f12ae8e2
c0113134 <c0113134d2@edu.teu.ac.jp>
2016-02-20 16:09:44 +0900

    abc commit

--- main.js
+++ main.js
@@ -4,27 +4,75 @@ var
     app = express(),
       port = process.env.PORT || 3000,
         parser = require('body-parser');
+	var knex = require('knex')({
+	client: 'sqlite3',
+	connection: {
+	filename: ":memory:"
+	}
+	});
 
         app.use(parser.urlencoded({ extended:false}));
 	app.use(parser.json());
 	app.use(express.static(__dirname + '/public'))
 
 	app.get('/api/projects', function (req, res, next) {
-	  res.json('実装するで～');
+	    knex.select('*').from('projects')
+	        .then(function (projects){
+	          res.json(projects);
+	          return next();
+	        })
+	        .catch(function (error){
+	          res.status(500).json(err);
+	          return next();
+	        });
+	});
+
+	app.get('/api/projects/:id', function (req, res, next) {
+	    var id = req.params.id;
+	    res.json(id);
 	    return next();
+	});
+
+	app.post('/api/badrequest', function (req, res, next) {
+	    var title = req.body.title,
+ 	        description = req.body.description,
+	        url = req.body.url;
+
+	    knex('projects').inserts({
+	       title: title,
+	       description: description,
+	       url: url
+	    }).then(function(ids){
+	       res.json({
+	         id: ids[0],
+		 title: title,
+		 description: description,
+		 url: url
+	       })
+	       return next();
+	    }).catch(function(err){
+	       res.status(500).json(err);
+	       return next();
 	    });
+	});
 
-	    app.get('/api/projects/:id', function (req, res, next) {
-	      var id = req.params.id;
-	      res.status(404).json('NotFound');
-	        return next();
-		});
+      /** @ToDo
+       * Initialize database
+	* this is for 'in-memory' database and should be removed
+	    */
+	var sqls = require('fs')
+	      .readFileSync(__dirname + '/specifications/database.sql')
+	      .toString();
 
-		app.post('/api/badrequest', function (req, res, next) {
-		  res.status(400).json('BadRequest');
-		    return next();
-		    })
+	knex.raw(sqls)
+	      .then(function () {
+		    /** @ToDo
+		     * Run server after database initialization
+	             * this is for 'in-memory' database and should be removed
+   	             */
+	  app.listen(port, function () {
+	      console.log("Server running with port", port)
+          });
+	});
 
-		    app.listen(port, function () {
-		      console.log('Server running with port', port);
-		      });
+
